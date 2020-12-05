2fb36d58a1d0eb1442badec0216950dd7cf1fa7a
Ryan Williams <ryanwilliamsdesktop@outlook.com>
2020-12-05 15:42:06 +0000

    recationg structs

--- GeneticAlgorithmOptimisation/GeneticAlgorithm.h
+++ GeneticAlgorithmOptimisation/GeneticAlgorithm.h
@@ -1,27 +1,34 @@
 #pragma once
 
+
+
+
 const int N = 50; //Numnber of genes
 const int P = 10; //population 
 const int GENERATIONS = 10;
 const float MUTRATE = 0.2;
 const float MUTSTEP = 1.0;
 
-
+//Each individual
 typedef struct {
 	float gene[N];
 	float fitness;
 } individual;
 
-enum SelectionType {ROULETTE, TOURNAMENT};
-
-
-void RunGeneticAlgorithm(SelectionType slectionType);
-
+//Each generation
 typedef struct {
 	int generation;
 	float bestFitness;
 	float meanFitness;
+} GenerationResult;
+
+typedef struct {
+	//TODO
 } GAResult;
 
-std::vector<GAResult> result;
 
+
+enum SelectionType { ROULETTE, TOURNAMENT };
+
+void RunGeneticAlgorithm(SelectionType slectionType);
+void RunGeneticAlgorithmWithLogging(SelectionType slectionType);
